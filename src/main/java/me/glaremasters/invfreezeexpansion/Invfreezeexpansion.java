1ff5350a1fb3007882b8f354a640a27348aa8fba
darbyjack <admin@glaremasters.me>
2018-03-02 14:20:08 -0600

    v1.0.0 expansion

---
+++ src/main/java/me/glaremasters/invfreezeexpansion/Invfreezeexpansion.java
@@ -0,0 +1,81 @@
+package me.glaremasters.invfreezeexpansion;
+
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
+import org.bukkit.entity.Player;
+
+/**
+ * This class will automatically register as a placeholder expansion
+ * when a jar including this class is added to the /plugins/placeholderapi/expansions/ folder
+ */
+public class Invfreezeexpansion extends PlaceholderExpansion {
+
+    /**
+     * This method should always return true unless we
+     * have a dependency we need to make sure is on the server
+     * for our placeholders to work!
+     * This expansion does not require a dependency so we will always return true
+     */
+    @Override
+    public boolean canRegister() {
+        return true;
+    }
+
+    /**
+     * The name of the person who created this expansion should go here
+     */
+    @Override
+    public String getAuthor() {
+        return "blockslayer22";
+    }
+
+    /**
+     * The placeholder identifier should go here
+     * This is what tells PlaceholderAPI to call our onPlaceholderRequest method to obtain
+     * a value if a placeholder starts with our identifier.
+     * This must be unique and can not contain % or _
+     */
+    @Override
+    public String getIdentifier() {
+        return "invfreeze";
+    }
+
+    /**
+     * if an expansion requires another plugin as a dependency, the proper name of the dependency should
+     * go here. Set this to null if your placeholders do not require another plugin be installed on the server
+     * for them to work
+     */
+    @Override
+    public String getPlugin() {
+        return null;
+    }
+
+    /**
+     * This is the version of this expansion
+     */
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    /**
+     * This is the method called when a placeholder with our identifier is found and needs a value
+     * We specify the value identifier in this method
+     */
+    @Override
+    public String onPlaceholderRequest(Player p, String identifier) {
+
+        // %example_placeholder1%
+        if (identifier.equals("status")) {
+            if (p.hasPermission("invfreeze.frozen")) {
+                return "frozen";
+            }
+            else {
+                return "unfrozen";
+            }
+        }
+
+        return null;
+    }
+}
